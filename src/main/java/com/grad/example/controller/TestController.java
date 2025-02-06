e4721714be8eb60b8d342abd271471646d0cf464
Kaushik-666 <ronnie_9211@rediffmail.com>
2025-02-07 00:22:11 +0530

    build.gradle updated

--- src/main/java/com/grad/example/controller/TestController.java
+++ src/main/java/com/grad/example/controller/TestController.java
@@ -1,14 +1,22 @@
 package com.grad.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class TestController {
-	
-	@GetMapping("/")
+
+	@GetMapping
 	public String getStringData() {
 		return "Hello World";
+	}
+
+	@GetMapping("/{name}")
+	public String getUserDetails(@PathVariable("name") String name) {
+		return "Hello " + name;
 	}
 
 }
